361e169c4a194fcbb3d8667cb2ce40c99bdbb681
Jonathan Kingsley <jon@kings.ly>
2021-01-31 20:53:47 +0000

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,34 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Image from "../components/image"
+
+const ProfileCard = () => (
+   <div className="p-96 max-w-m mx-auto bg-white rounded-lg shadow-md flex items-center space-x-4">
+   <Image className="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0" alt="Jon Kingsley" filename="profile.png" />
+    <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0" src="/images/profile.png" alt="Woman's Face"></img>
+    <div className="text-center space-y-2 sm:text-left">
+      <div className="space-y-0.5">
+        <p className="text-lg text-black font-semibold">
+          Erin Lindford
+        </p>
+        <p className="text-gray-500 font-medium">
+          Product Engineer
+        </p>
+      </div>
+    </div>
+  </div>
+)
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="w-screen h-screen bg-gradient-to-r from-pink-500 via-red-500 to-yellow-400 flex items-center justify-center h-screen">
+        <ProfileCard/>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+
   </Layout>
 )
 
